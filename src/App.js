fabc8068d0164bef221cbc761f2545cb719f6629
harrypmdev <harrypetermiles@gmail.com>
2025-03-07 18:53:58 +0000

    feat: add home component and basic home layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import { Route, Routes } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
+import Home from './components/Home';
+import styles from './styles/App.module.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className={`${styles.roboto} min-vh-100 d-flex flex-column`}>
       <NavBar />
-      <Container>
+      <Container className="d-flex flex-column flex-grow-1">
         <Routes>
-          <Route path="/" element={<p>Home Page</p>}/>
+          <Route path="/" element={<Home />}/>
           <Route path="/second/" element={<p>Second Page</p>}/>
         </Routes>
       </Container>
---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
+const Home = () => {
+  return (
+    <Container className="flex-grow-1 d-flex flex-column">
+        <Row className="d-flex flex-grow-1 align-items-center pb-6">
+            <Col className='col-6 text-center'>
+                <h1 className="h2 mb-3">Worthing's Music Hub</h1>
+                <p className="mb-4">
+                Worthing Sound Wave is a site for local Worthing musicians to discuss music,
+                collaborate or share new tracks. You can find bandmates, emerging artists and events.
+                </p>
+                <Button variant="primary me-3">Register</Button>
+                <Button variant="secondary ms-3">See What's On</Button>
+            </Col>
+            <Col className='col-6 text-center'>Part 2</Col>
+        </Row>
+    </Container>
+  )
+}
+
+export default Home
