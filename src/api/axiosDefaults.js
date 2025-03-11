f612e21c0932c102a2d734eeed92534b818f383f
harrypmdev <harrypetermiles@gmail.com>
2025-03-11 12:57:23 +0000

    feat: add basic config vars to axiosDefaults.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,1 +1,5 @@
 import axios from 'axios';
+
+axios.defauts.baseURL = 'https://worthing-sw-api-fc5a7cd44915.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
