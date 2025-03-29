4e98f991f6175aa0ccc57ac1a73d033dc9f22c71
harrypmdev <harrypetermiles@gmail.com>
2025-03-29 16:28:18 +0000

    feat: add docstring to SongList.js

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,9 +3,14 @@ import { Container } from 'react-bootstrap';
 
 import Song from './Song';
 
-
+/**
+ * Render a list of songs ranked from 1-3 by vote popularity.
+ * 
+ * @param {Object} profile An object containing profile details as retrieved from the '/profiles' endpoint.
+ * @param {Object} songData An object containing a list of 'song' objects as retrieved from the '/songs' endpoint.
+ * @returns {ReactNode} - An element displaying a list of ranked songs as returned from the '/songs' endpoint. 
+ */
 const SongList = ({profile, songData}) => {
-
   return (
     <Container className="flex-grow-1 d-flex flex-column">
       { songData.length && (
