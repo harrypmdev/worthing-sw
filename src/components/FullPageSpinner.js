6fd2fff300d7be3c2cb3967d7a5d8a9e6eda5849
harrypmdev <harrypetermiles@gmail.com>
2025-03-29 15:33:22 +0000

    feat: add docstring to FullPageSpinner.js

--- src/components/FullPageSpinner.js
+++ src/components/FullPageSpinner.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Container, Spinner } from 'react-bootstrap'
 
+/**
+ * Renders a loading spinner that will fill the whole page.
+ * 
+ * @returns {ReactNode} An element displaying a full screen loading spinner.
+ */
 const FullPageSpinner = () => {
   return (
     <Container className="flex-grow-1 d-flex flex-column justify-content-center align-items-center">
